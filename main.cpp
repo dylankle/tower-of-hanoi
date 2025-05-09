853a723e4931a4b686563f42d1cf982e37f3ebac
Dylan Le <68560708+dylankle@users.noreply.github.com>
2025-05-09 12:38:55 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,12 +16,12 @@ void solveHanoi(int n, string &source, string &extra, string &target, int &curre
 
     cout << "Step " << currentMove++ << ". Move disc " << n << " from " << source << " to " << target << endl; //move the top disc from source peg to the end peg
 
-    solveHanoi(n - 1, extra, source, target, currentMove); //after the first half of recursion finishes, we have to take care of the next disc's new source which was the extra peg
+    solveHanoi(n - 1, extra, source, target, currentMove); //after the first half of recursion finishes, we have to take care of the next disc's new source which is now the extra peg
 }
 
 int main() {
-    int n; //number of disc you want this game to have
-    string source = "left", extra = "middle", target = "right"; //the left peg is the start aka the source of all discs. the middle peg is extra because the right peg is where all the discs should end.
+    int n; //number of discs you want this game to have
+    string source = "left", extra = "middle", target = "right"; //the left peg is the start aka the source of all discs. the middle peg is extra because the right peg is where all the discs should end
     int currentMove = 1; //move counter to compare the count of the final step vs calculated minimum moves from the formula
 
     cout << "Enter the number of discs: ";
